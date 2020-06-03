0f33491a292bbd6afe99674a50d77d6ef16fa531
manzhaimaksim <maxbbb88@gmail.com>
2020-06-03 21:02:36 +0300

    generate model & controler for Event

---
+++ app/models/event.rb
@@ -0,0 +1,8 @@
+class Event < ApplicationRecord
+
+  validates :title, presence: true,
+                    length: { maximum: 255 }
+
+  validates :address, presence: true
+  validates :datetime, presence: true
+end

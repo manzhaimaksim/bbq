9f387a01b0d092b4eb3e04c72000984ff61a7104
manzhaimaksim <maxbbb88@gmail.com>
2020-06-17 21:56:42 +0300

    generate controller Subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -7,6 +7,12 @@ class Subscription < ApplicationRecord
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
 
+  # Для конкретного event_id один юзер может подписаться только один раз (если юзер задан)
+  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
+
+  # Или один email может использоваться только один раз (если анонимная подписка)
+  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+
   def user_name
     if user.present?
       user.name

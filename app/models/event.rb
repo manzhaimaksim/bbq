be70109d2a758a8ce208ea9baaa3ac3e50ccd2d8
manzhaimaksim <maxbbb88@gmail.com>
2020-06-17 21:45:20 +0300

    generate model subscription

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   belongs_to :user
 
   has_many :comments, dependent: :destroy
+  has_many :subscriptions
 
   validates :title, presence: true,
                     length: { maximum: 255 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   has_many :events
   has_many :comments, dependent: :destroy
+  has_many :subscriptions
 
   validates :name,  presence: true,
                     length: { maximum: 35 }

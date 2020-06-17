67c3e48a6af4bade63d82c3c5fec8ca5163ac147
manzhaimaksim <maxbbb88@gmail.com>
2020-06-17 22:35:01 +0300

    renew subscriptions if the user is registered

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
 
   validates :name,  presence: true,
                     length: { maximum: 35 }
+
+  after_commit :link_subscriptions, on: :create
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email).update_all(user_id: self.id)
+  end
 end

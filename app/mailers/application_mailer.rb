2bc0c0b272f5a440b741a7d8712105fb61059f26
manzhaimaksim <maxbbb88@gmail.com>
2020-07-03 21:11:34 +0300

    edit default email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'testing.goodp@gmail.com'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -129,7 +129,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'barbie-queue.herokuapp.com',
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 end

f15263d755c9fce31be9b1fac5261e8818091d96
manzhaimaksim <maxbbb88@gmail.com>
2020-07-03 21:47:28 +0300

    small fix2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -125,8 +125,8 @@ Rails.application.configure do
   # А это для SendGrid
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => 587,
-    :authentication => :login,
+    :port           => 465,
+    :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',

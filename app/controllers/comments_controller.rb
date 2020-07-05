22d7406fb1c9ec4882bd2f0a7d857f5f6d5264f6
manzhaimaksim <maxbbb88@gmail.com>
2020-07-05 17:02:19 +0300

    comment author does not recieve a letter about him

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -44,7 +44,7 @@ class CommentsController < ApplicationController
   def notify_subscribers(event, comment)
     # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
-
+    all_emails.delete(comment.user.email)
     # По адресам из этого массива делаем рассылку
     # Как и в подписках, берём EventMailer и его метод comment с параметрами
     # И отсылаем в том же потоке

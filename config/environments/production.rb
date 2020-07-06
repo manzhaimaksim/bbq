71b6343f11aa5a8bc780979fac3b0a082bff900d
manzhaimaksim <maxbbb88@gmail.com>
2020-07-06 21:57:44 +0300

    edit settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,11 +124,11 @@ Rails.application.configure do
 
   # А это для SendGrid
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
+    :address        => 'smtp.mailgun.org',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['MAILGUN_USERNAME'],
+    :password       => ENV['MAILGUN_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }

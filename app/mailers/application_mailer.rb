a887fa9c4d53d9fbd00f15d85d3b65b1fd80e939
manzhaimaksim <maxbbb88@gmail.com>
2020-07-06 22:04:00 +0300

    edit default email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'testing.goodp@gmail.com'
+  default from: 'postmaster@sandboxc584c512ad214304b3ce3c40ea699f55.mailgun.org'
   layout 'mailer'
 end

577c7b0a8f01a6dc7ace21ddcc6356a9683e1ea8
manzhaimaksim <maxbbb88@gmail.com>
2020-07-30 22:53:10 +0300

    editing gallery

--- app/javascript/packs/gallery.js
+++ app/javascript/packs/gallery.js
@@ -1,4 +1,4 @@
-$(document).ready(function( $ ) {
+$(document).on('turbolinks:load', function() {
   $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();

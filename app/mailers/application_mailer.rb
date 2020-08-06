6fc18c584b2e148070bca7d3348e86c1d7703878
manzhaimaksim <maxbbb88@gmail.com>
2020-08-07 01:29:59 +0300

    edit config for sendmail part 3

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'testing.goodp@gmail.com'
+  default from: 'app180309703@heroku.com'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,14 +122,13 @@ Rails.application.configure do
   # Устанавливаем протокол, по которому отправлять (SMTP)
   config.action_mailer.delivery_method = :smtp
 
-  # А это для SendGrid
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => '587',
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => 'heroku.com',
-  #   :enable_starttls_auto => true
-  # }
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_API_KEY'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end

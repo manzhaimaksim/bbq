1838889351d3159e69ff3581fb124c2e0a6adc7d
manzhaimaksim <maxbbb88@gmail.com>
2020-08-06 13:03:55 +0300

    adding air-dataPicker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,6 +23,7 @@ import '@fortawesome/fontawesome-free/scss/v4-shims';
 
 import "@fortawesome/fontawesome-free/js/all";
 import './gallery'
+import './datepicker.min'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

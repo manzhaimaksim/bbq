57e2bd521df82e2426a137a3b8ecd32c09ceb603
manzhaimaksim <maxbbb88@gmail.com>
2020-08-07 17:01:59 +0300

    edit config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -123,12 +123,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-    user_name: ENV['GMAIL_USER_NAME'],
-    password: ENV['GMAIL_PASSWORD'],
-    domain: 'heroku.com',
-    address: 'smtp.gmail.com',
-    port: '587',
-    authentication: :plain,
-    enable_starttls_auto: true
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_API_KEY'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 end

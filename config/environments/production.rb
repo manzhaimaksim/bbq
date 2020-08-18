f74041b4acf3d1a6c4cea009dda0fe7d63fec6e3
manzhaimaksim <maxbbb88@gmail.com>
2020-08-18 21:28:33 +0300

    add mailgun options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,15 +120,13 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   # Устанавливаем протокол, по которому отправлять (SMTP)
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    authentication: 'plain',
-    domain: 'heroku.com',
-    enable_starttls_auto: true
+  ActionMailer::Base.smtp_settings = {
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'barbiequeue.heroku.com',
+    authentication: :plain
   }
+  ActionMailer::Base.delivery_method = :smtp
 end

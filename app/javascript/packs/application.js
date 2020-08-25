91e39cc6e5b9e823262d867eb3c7524fdf888a12
manzhaimaksim <maxbbb88@gmail.com>
2020-08-25 22:16:13 +0300

    adding datetimepicker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import "bootstrap";
 import "jquery"
 import "../stylesheets/application";
@@ -14,21 +9,14 @@ require("@rails/activestorage").start()
 require("channels")
 require("bootstrap")
 require("popper.js")
-require("air-datepicker")
 
 import '@fortawesome/fontawesome-free/scss/fontawesome';
 import '@fortawesome/fontawesome-free/scss/solid';
 import '@fortawesome/fontawesome-free/scss/regular';
 import '@fortawesome/fontawesome-free/scss/brands';
 import '@fortawesome/fontawesome-free/scss/v4-shims';
+import 'air-datepicker/dist/js/datepicker.min'
 
 import "@fortawesome/fontawesome-free/js/all";
 import './gallery'
-// import './datepicker.min'
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+import './datepicker'

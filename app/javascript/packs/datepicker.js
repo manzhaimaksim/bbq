df48a2850d52ecc33e128b6dd7a0088cb0a4ee4e
manzhaimaksim <maxbbb88@gmail.com>
2020-09-02 20:54:14 +0300

    edit validation for datetimepicker

--- app/javascript/packs/datepicker.js
+++ app/javascript/packs/datepicker.js
@@ -1,4 +1,7 @@
 $(document).on('turbolinks:load', function() {
+  let time = new Date()
+  time.setHours(time.getHours() + 1)
+
   $('#event_datetime').datepicker({
     timepicker: true,
     minDate: new Date()

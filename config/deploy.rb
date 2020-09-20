5ada3584ac832fc201a9dd929ab90167302c58b1
manzhaimaksim <maxbbb88@gmail.com>
2020-09-20 21:42:39 +0300

    change ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.14.1"
 set :application, 'bbq'
 set :repo_url, 'git@github.com:manzhaimaksim/bbq.git'
 set :deploy_to, '/home/deploy/apps/bbq'
-append :linked_files, 'config/application.yml'
+append :linked_files, 'config/application.yml', 'config/master.key'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
 
 # Default branch is :master

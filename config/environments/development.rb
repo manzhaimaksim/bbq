7dc6f462a15b9e44f8c2b143565668dbb1f22f43
manzhaimaksim <maxbbb88@gmail.com>
2020-10-03 23:49:03 +0300

    adding gem rescue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.active_job.queue_adapter = :resque
+  config.active_job.queue_buname_prefix = "bbq_#{Rails.env}"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on

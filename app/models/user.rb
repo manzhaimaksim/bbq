a5d54eb0ac0a456c96939f71715e0ada3830870f
manzhaimaksim <maxbbb88@gmail.com>
2020-10-07 22:07:16 +0300

    adding login with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   before_create :add_unsubscribe_hash
 
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable,
+         omniauth_providers: [:facebook]
 
   has_many :events, dependent: :destroy
   has_many :comments, dependent: :destroy
@@ -18,6 +19,28 @@ class User < ApplicationRecord
 
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
+  end
+
+  def self.find_for_facebook_oauth(access_token)
+    # Достаём email из токена
+    email = access_token.info.email
+    user = where(email: email).first
+
+    # Возвращаем, если нашёлся
+    return user if user.present?
+
+    # Если не нашёлся, достаём провайдера, айдишник и урл
+    provider = access_token.provider
+    id = access_token.extra.raw_info.id
+    url = "https://facebook.com/#{id}"
+
+    # Теперь ищем в базе запись по провайдеру и урлу
+    # Если есть, то вернётся, если нет, то будет создана новая
+    where(url: url, provider: provider).first_or_create! do |user|
+      # Если создаём новую запись, прописываем email и пароль
+      user.email = email
+      user.password = Devise.friendly_token.first(16)
+    end
   end
 
   private
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
     authentication: 'plain',
     enable_starttls_auto: true
   }
+
+  config.hosts << "lvh.me"
 end

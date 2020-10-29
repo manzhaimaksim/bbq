98380dfb277d599c023f10c9203129745ff53d08
manzhaimaksim <maxbbb88@gmail.com>
2020-10-29 21:01:59 +0300

    update settings for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,17 +112,17 @@ Rails.application.configure do
 
   # test capistrano
 
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'afternoon-fortress-30762.herokuapp.com' }
 
   ActionMailer::Base.smtp_settings = {
-      address: 'smtp.mailgun.org',
-      port: '587',
-      authentication: :plain,
-      user_name: ENV['MAILGUN_USERNAME'],
-      password: ENV['MAILGUN_PASSWORD'],
-      domain: 'heroku.com',
-      enable_starttls_auto: true
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'afternoon-fortress-30762.herokuapp.com',
+    :authentication => :plain,
   }
+  ActionMailer::Base.delivery_method = :smtp
 end

8abe99644391eed8c9039f7081db0d455be6310f
manzhaimaksim <maxbbb88@gmail.com>
2020-11-04 19:47:39 +0300

    some fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, 'bbq'
+set :application, 'barbiequeue'
 set :repo_url, 'git@github.com:manzhaimaksim/bbq.git'
-set :deploy_to, '/home/deploy/apps/bbq'
+set :deploy_to, '/home/deploy/apps/barbiequeue'
 append :linked_files, 'config/application.yml', 'config/master.key'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
 # after 'deploy:updated', 'webpacker:precompile'

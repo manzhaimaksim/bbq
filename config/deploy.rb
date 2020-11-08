8b67e610719e61bf3253475309840598ec9672c9
manzhaimaksim <maxbbb88@gmail.com>
2020-11-08 22:28:04 +0300

    edit config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,11 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, 'bbq'
+set :application, 'barbiequeue'
 set :repo_url, 'git@github.com:manzhaimaksim/bbq.git'
-set :branch, "lesson70/facebook_oauth"
-set :deploy_to, '/home/deploy/apps/bbq'
+set :branch, "lesson70/refactor_oauth"
+set :deploy_to, '/home/deploy/apps/barbiequeue'
+
 append :linked_files, 'config/application.yml', 'config/master.key'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
 # after 'deploy:updated', 'webpacker:precompile'
